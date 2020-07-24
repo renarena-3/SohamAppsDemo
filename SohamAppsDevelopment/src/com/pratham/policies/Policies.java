440c1e2b20b03baa0b74ca25bbcf7e57f6cf9037
tl <tl@gmail.com>
2020-07-24 17:25:51 +0530

    Crud Methods are added by the Developer2 In policy module

--- SohamAppsDevelopment/src/com/pratham/policies/Policies.java
+++ SohamAppsDevelopment/src/com/pratham/policies/Policies.java
@@ -7,4 +7,20 @@ public class Policies {
 		
 		return null;
 	}
+	
+	public String updatePolicy() {
+		
+		return null;
+	}
+	
+	public String fetchPolicy() {
+		
+		return null;
+		
+	}
+	
+	public String deletePolicy() {
+		
+		return null;
+	}
 }
