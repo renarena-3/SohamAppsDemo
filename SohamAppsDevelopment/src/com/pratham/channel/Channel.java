d7b34100b8f0b1988e6ea8c7c3c0d50746e564b4
tl <tl@gmail.com>
2020-07-24 17:18:46 +0530

    In Channel Crud methods are added by the Developer2

--- SohamAppsDevelopment/src/com/pratham/channel/Channel.java
+++ SohamAppsDevelopment/src/com/pratham/channel/Channel.java
@@ -6,4 +6,24 @@ public class Channel {
 		
 		return "Hello-Channels";
 	}
+	
+	public String createChannel() {
+		
+		return null;
+	}
+	
+	public String deleteChannel() {
+		
+		return null;
+	}
+	
+	public String updateChannel() {
+		
+		return null;
+	}
+	
+	public String fetchAllChannels() {
+		
+		return null;
+	}
 }
